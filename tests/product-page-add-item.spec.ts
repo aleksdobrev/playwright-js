ed30389075bca2f44bba7846e540aec5027eefb6
aleksdobrev <aleksdobrev87@gmail.com>
2024-03-25 21:33:12 +0200

    add assertions to test

--- tests/product-page-add-item.spec.ts
+++ tests/product-page-add-item.spec.ts
@@ -1,11 +1,21 @@
 import { expect, test } from "@playwright/test";
+import exp from "constants";
 
 test("Product Page Add to Basket", async ({ page }) => {
   await page.goto("http://localhost:2221/");
 
-  const addToBasketButton = page.getByRole("button", { name: "Add to Basket" }).first();
+  const addToBasketButton = page.locator('[data-qa="product-button"]').first();
+  const basketCounter = page.locator('[data-qa="header-basket-count"]');
+
   await addToBasketButton.waitFor();
+  await basketCounter.waitFor();
+  await expect(addToBasketButton).toHaveText("Add to Basket");
+  await expect(basketCounter).toHaveText("0");
+
   await addToBasketButton.click();
+
+  await expect(addToBasketButton).toHaveText("Remove from Basket");
+  await expect(basketCounter).toHaveText("1");
 
   // await page.pause();
 });
