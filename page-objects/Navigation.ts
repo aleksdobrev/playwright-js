c8cd8227d82248a0151a03f6c027a6b3a88f2a4e
aleksdobrev <aleksdobrev87@gmail.com>
2024-03-28 11:38:21 +0200

    add goToCheckout method

--- page-objects/Navigation.ts
+++ page-objects/Navigation.ts
@@ -1,17 +1,25 @@
-import { type Locator, type Page } from "@playwright/test";
+import { type Locator, type Page, expect } from "@playwright/test";
 
 export class Navigation {
   readonly page: Page;
   readonly basketCounter: Locator;
+  readonly checkoutLink: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.basketCounter = page.locator('[data-qa="header-basket-count"]');
+    this.checkoutLink = page.getByRole("link", { name: "Checkout" });
   }
 
   async getBasketCount() {
     await this.basketCounter.waitFor();
     const text = await this.basketCounter.innerText();
     return parseInt(text, 10);
+  }
+
+  async goToCheckout() {
+    await this.checkoutLink.waitFor();
+    await this.checkoutLink.click();
+    await expect(this.page).toHaveURL("/basket");
   }
 }
