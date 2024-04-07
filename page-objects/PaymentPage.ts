2d90e5bb586fad5c2c26eb7a9b9def92183e4583
aleksdobrev <aleksdobrev87@gmail.com>
2024-04-07 21:20:02 +0300

    assert discounts

--- page-objects/PaymentPage.ts
+++ page-objects/PaymentPage.ts
@@ -5,12 +5,18 @@ export class PaymentPage {
   readonly discountCode: Locator;
   readonly discountCodeInput: Locator;
   readonly submitDiscountCodeButton: Locator;
+  readonly discountActiveMessage: Locator;
+  readonly discountedPrice: Locator;
+  readonly totalPrice: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.discountCode = page.frameLocator('[data-qa="active-discount-container"]').locator('[data-qa="discount-code"]');
     this.discountCodeInput = page.locator('[data-qa="discount-code-input"]');
     this.submitDiscountCodeButton = page.locator('[data-qa="submit-discount-button"]');
+    this.discountActiveMessage = page.locator('[data-qa="discount-active-message"]');
+    this.discountedPrice = page.locator('[data-qa="total-with-discount-value"]');
+    this.totalPrice = page.locator('[data-qa="total-value"]');
   }
 
   async activateDiscount() {
@@ -27,7 +33,19 @@ export class PaymentPage {
     // await this.page.keyboard.type(code, { delay: 500 });
 
     await this.submitDiscountCodeButton.waitFor();
+    await expect(this.discountActiveMessage).not.toBeVisible();
+    await expect(this.discountedPrice).not.toBeVisible();
     await this.submitDiscountCodeButton.click();
-    await this.page.pause();
+    await expect(this.discountActiveMessage).toBeVisible();
+    await expect(this.discountActiveMessage).toHaveText("Discount activated!");
+    await expect(this.discountedPrice).toBeVisible();
+    const discountedTotalText = await this.discountedPrice.innerText();
+    const discountWithoutDollarSign = discountedTotalText.replace("$", "");
+    const discountTotalNumber = parseInt(discountWithoutDollarSign, 10);
+    await this.totalPrice.waitFor();
+    const regularPriceText = await this.totalPrice.innerText();
+    const regularWithoutDollarSign = regularPriceText.replace("$", "");
+    const regularPriceNumber = parseInt(regularWithoutDollarSign, 10);
+    expect(discountTotalNumber).toBeLessThan(regularPriceNumber);
   }
 }
