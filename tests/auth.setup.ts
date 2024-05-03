946d7a3837fe324b59c9f259b4c451cc5c440b6a
Alex Dobrev <aleksandar.dobrev@gomomentus.com>
2024-05-03 09:33:10 +0300

    use dotenv

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -4,8 +4,8 @@ const authFile = "auth.json";
 
 test("auth", async ({ page }) => {
   await page.goto("/login?redirect=/my-account");
-  await page.getByPlaceholder("E-Mail").fill("admin");
-  await page.getByPlaceholder("Password").fill("Admin123");
+  await page.getByPlaceholder("E-Mail").fill(process.env.USER);
+  await page.getByPlaceholder("Password").fill(process.env.ADMIN_PASSWORD);
   await page.getByRole("button", { name: "Login" }).click();
   await page.waitForSelector("[data-qa='delivery-address']");
   await page.context().storageState({ path: authFile });
--- tests/my-account.spec.ts
+++ tests/my-account.spec.ts
@@ -4,5 +4,4 @@ import { MyAccountPage } from "../page-objects/MyAccountPage";
 test.only("Visit My Account using cookie injection", async ({ page }) => {
   const myAccount = new MyAccountPage(page);
   await myAccount.visit();
-  await page.pause();
 });
