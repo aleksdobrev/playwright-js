74e7921259e7384c450cd8d5bec7fe769178e42b
Alex Dobrev <aleksandar.dobrev@gomomentus.com>
2024-05-05 22:26:27 +0300

    remove forbidOnly

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   globalSetup: require.resolve("./auth.setup.ts"),
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
